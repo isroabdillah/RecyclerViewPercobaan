b752af710a37db6ee8c878ab28eb3074afd05ca7
isroabdillah <isroabdillah.dev@gmail.com>
2023-04-21 03:36:48 +0700

    RecyclerViewExample | Adapter, Model, Layout Item, Layout Manager

--- app/src/main/java/com/isroabdillah/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/isroabdillah/recyclerviewexample/MainActivity.kt
@@ -2,10 +2,47 @@ package com.isroabdillah.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvsuperhero: RecyclerView
+
+    private val list = ArrayList<SuperHero>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvsuperhero = findViewById(R.id.rv_superhero)
+        rvsuperhero.setHasFixedSize(true)
+
+        list.addAll(getListSuperHero())
+
+//        rvsuperhero.layoutManager = LinearLayoutManager(this)
+//        val superHeroAdapter = SuperHeroAdapter(list)
+//        rvsuperhero.adapter = superHeroAdapter
+
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvsuperhero.layoutManager = LinearLayoutManager(this)
+        val superHeroAdapter = SuperHeroAdapter(list)
+        rvsuperhero.adapter = superHeroAdapter
+    }
+
+    private fun getListSuperHero(): ArrayList<SuperHero> {
+        val name = resources.getStringArray(R.array.data_name)
+        val desc = resources.getStringArray(R.array.data_desc)
+
+        val listSuperHero = ArrayList<SuperHero>()
+        for (i in name.indices){
+            val superHero = SuperHero(
+                name[i], desc[i]
+            )
+            listSuperHero.add(superHero)
+        }
+        return listSuperHero
     }
 }
--- app/src/main/java/com/isroabdillah/recyclerviewexample/SuperHeroAdapter.kt
+++ app/src/main/java/com/isroabdillah/recyclerviewexample/SuperHeroAdapter.kt
@@ -13,6 +13,24 @@ class SuperHeroAdapter(private val list: ArrayList<SuperHero>) : RecyclerView.Ad
         val tvDescription: TextView = itemView.findViewById(R.id.tv_desc)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        return SuperHeroViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+        val (name, description) = list[position]
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+    }
+
+    override fun getItemCount(): Int = list.size
+
+//    class SuperHeroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val tvName: TextView = itemView.findViewById(R.id.tv_name)
+//        val tvDescription: TextView = itemView.findViewById(R.id.tv_desc)
+//    }
+//
 //    inner class SuperHeroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 //        fun bind(superHero: SuperHero){
 //            with(itemView){
@@ -21,19 +39,19 @@ class SuperHeroAdapter(private val list: ArrayList<SuperHero>) : RecyclerView.Ad
 //            }
 //        }
 //    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
-        return SuperHeroViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
-//        holder.bind(list[position])
-        val (name, description) = list[position]
-        holder.tvName.text = name
-        holder.tvDescription.text = description
-    }
-
-    override fun getItemCount(): Int = list.size
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+//        return SuperHeroViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+////        holder.bind(list[position])
+//        val (name, description) = list[position]
+//        holder.tvName.text = name
+//        holder.tvDescription.text = description
+//    }
+//
+//    override fun getItemCount(): Int = list.size
 
 }
